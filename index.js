7c5bdfb2b43ff9ec106282c101c68e099c6d8f2d
simplyComplex <91500514+Simplycomplex64@users.noreply.github.com>
2024-04-22 06:14:39 -0400

    5th commit

---
+++ index.js
@@ -0,0 +1,18 @@
+/* <div class="single-item">
+<img src="/images/pizza.png" alt="a slice of pizza">
+</div>
+<div class="item-description">
+<h2>Pizza</h2>
+<p>Peperoni, mushrooms, mozarella</p>
+<p>$14</p>
+</div>
+<div class="add-btn">
+<img src="/images/JJJ’s (5).png" alt="add button">
+</div>  */
+
+
+// <!-- <div class="order-details">
+//                     <h3>Pizza</h3>
+//                     <span>remove</span>
+//                     <p>$14</p>
+//                 </div> -->
